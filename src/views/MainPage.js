64c91090b0ea99129e8ebb7b0c3dc8d63d4e0537
a-ssh1ey <annasoboleva0507@gmail.com>
2024-07-13 15:51:27 +0300

    PUSH3.1

--- src/views/MainPage.js
+++ src/views/MainPage.js
@@ -4,7 +4,6 @@ import  Footer from "../components/Footer/Footer";
 function MainPage({setActive, selected}) {
     return (
         <div>
-            <h1>Main Page</h1>
             <Footer setActive={setActive} selected={selected}></Footer>
         </div>
     );
